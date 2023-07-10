9cac83f51071304581f3012df9f7b6b4fe1c76ef
aditya <meenakshi.bhtt@gmail.com>
2023-07-10 08:25:18 +0530

    Data Preprocessing continued

--- Src/Data/data_preprocessing.py
+++ Src/Data/data_preprocessing.py
@@ -1,19 +1,17 @@
 from data_loader import get_data
 import pandas as pd
 import numpy as np
+from datetime import datetime
 data = get_data("Data/Raw/train.csv")
 
 
 def preprocess_data(data: pd.DataFrame)->pd.DataFrame:
     '''
     The function will return the preprocessed data
-
     Input-
     Pandas data
-
     Output-
     Pandas data
-
     '''
     try:
         # Add Preprocessing Steps Here
@@ -37,6 +35,57 @@ def preprocess_data(data: pd.DataFrame)->pd.DataFrame:
             if data[i].dtype == "float64" and data[i].isnull().sum() > 0:
                 mean = data[i].mean()
                 data[i].fillna(mean, inplace=True)
+        #Utilities modelling
+
+        utilities_mapping={
+            "AllPub":4,
+            "NoSewr":3,
+            "NoSeWa":2,
+            "ELO":1
+        }
+        data['Utilities'].replace(utilities_mapping,inplace=True)
+        #LandSlope Modelling
+        landslope_modelling ={
+            "Gtl":0,
+            "Mod":1,
+             "Sev":2
+        }
+        data['LandSlope'].replace(landslope_modelling,inplace=True)
+        #OverallQual Modelling
+        rating_overall_qual = {
+            'Very Excellent': 10,
+            'Excellent': 9,
+            'Very Good': 8,
+            'Good': 7,
+            'Above Average': 6,
+            'Average': 5,
+            'Below Average': 4,
+            'Fair': 3,
+            'Poor': 2,
+            'Very Poor': 1
+        }
+
+
+        data['OverallQual'].replace(rating_overall_qual,inplace=True)
+        #OverallCond Modelling (Will use the same Previous Dictionary)
+        data['OverallCond'].replace(rating_overall_qual,inplace=True)
+        #Creating a new column as overall state
+        data['OverallState']=(data['OverallQual']+data['OverallCond'])//2
+        #Droping OverallQual and QverallCond
+        data.drop(['OverallQual','OverallCond'],inplace=True,axis=1)
+        #Computing age of house  using YearBuilt: Original construction date
+        current_year = datetime.now().year
+        data['Age_of_House'] = current_year - data['YearBuilt']
+        #Droping Year Bulit Column
+        data.drop('YearBuilt',inplace=True,axis=1)
+        #The distinct value in Utilities are --Remove in Prod
+        print("The distinct value in Utilities are ",data['Utilities'].unique())
+        #The distinct value in land slope are --Remove in Prod
+        print("The distinct value in Land Slope are ",data['LandSlope'].unique())
+        #The distinct value in OverallState --Remove in Prod
+        print("The distinct value in OverallState are ",data['OverallState'].unique())
+        #The distinct value in age of House
+        print("The distinct value in age of House is ",data['Age_of_House'].unique())
         #Check if any values are there --Remove it in Prod
         data_meta=pd.DataFrame(data.isnull().sum()/len(data)).reset_index().rename(columns={0:"Nulls"})
         data_meta=data_meta[data_meta['Nulls']>0]
@@ -47,4 +96,4 @@ def preprocess_data(data: pd.DataFrame)->pd.DataFrame:
     except Exception as e:
         return e
     
-preprocess_data(data)
+print(preprocess_data(data))
